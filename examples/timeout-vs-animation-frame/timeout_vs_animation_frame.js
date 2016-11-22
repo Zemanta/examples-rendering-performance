46e469e4a15a5a155ae5936e8bff5c72b5edad3a
Jure Bajt <jure.bajt@gmail.com>
2016-11-22 14:11:17 +0100

    Refactored folder structure a bit more.

--- examples/timeout-vs-animation-frame/timeout_vs_animation_frame.js
+++ examples/timeout-vs-animation-frame/timeout_vs_animation_frame.js
@@ -1,90 +1,91 @@
-function init () {
-    var controlButtons = document.querySelectorAll('#controls button');
+(function () {
+    function init () {
+        var controlButtons = document.querySelectorAll('#controls button');
 
-    controlButtons.forEach((button) => {
-        button.addEventListener('click', (event) => {
-            setActiveButton(event.target);
-            executeAction(event.target.dataset.action);
+        controlButtons.forEach((button) => {
+            button.addEventListener('click', (event) => {
+                setActiveButton(event.target);
+                executeAction(event.target.dataset.action);
+            });
         });
-    });
-}
-
-function executeAction (action, ...params) {
-    switch (action) {
-    case 'restartSt': return restartSt();
-    case 'restartAf': return restartAf();
     }
-}
 
-var st = {
-    el: document.getElementById('st'),
-    len: 0,
-    timeLabel: document.getElementById('timeSt'),
-};
-var af = {
-    el: document.getElementById('af'),
-    len: 0,
-    timeLabel: document.getElementById('timeAf')
-};
-
-function work (workLength) {
-    var start = (new Date()).getTime();
-    while((new Date()).getTime() - start < workLength){}
-}
-
-function render (obj, workLength, startTime){
-    if (obj.len > 300){
-        obj.timeLabel.innerHTML = `${((new Date()).getTime() - startTime)}ms`;
-        return false;
+    function executeAction (action, ...params) {
+        switch (action) {
+        case 'restart': return restartSt();
+        }
     }
-    work(workLength);
-    obj.el.style.width = `${obj.len}px`;
-    obj.len += 4;
-    return true;
-}
 
-function restartSt () {
-    if (st.len > 0 && st.len < 300) return;
-    var workLength = parseInt(document.getElementById('workLength').value) || 0;
-    var startTime = (new Date()).getTime();
-    st.len = 0;
-    st.timeLabel.innerHTML = '';
-    stLoop(st, workLength, startTime);
-}
+    var st = {
+        el: document.getElementById('st'),
+        len: 0,
+        timeLabel: document.getElementById('timeSt'),
+    };
+    var af = {
+        el: document.getElementById('af'),
+        len: 0,
+        timeLabel: document.getElementById('timeAf')
+    };
 
-function restartAf () {
-    if (af.len > 0 && af.len < 300) return;
-    var workLength = parseInt(document.getElementById('workLength').value) || 0;
-    var startTime = (new Date()).getTime();
-    af.len = 0;
-    af.timeLabel.innerHTML = '';
-    afLoop(af, workLength, startTime);
-}
-
-function stLoop (st, workLength, startTime) {
-    if (!render(st, workLength, startTime)) {
-        restartAf();
-        return false;
+    function work (workLength) {
+        var start = (new Date()).getTime();
+        while((new Date()).getTime() - start < workLength){}
     }
-    setTimeout(() => {
-        stLoop(st, workLength, startTime)
-    }, 16.667);
-}
 
-function afLoop (af, workLength, startTime) {
-    if (!render(af, workLength, startTime)) {
-        return false;
+    function render (obj, workLength, startTime){
+        if (obj.len > 300){
+            obj.timeLabel.innerHTML = `${((new Date()).getTime() - startTime)}ms`;
+            return false;
+        }
+        work(workLength);
+        obj.el.style.width = `${obj.len}px`;
+        obj.len += 4;
+        return true;
     }
-    requestAnimationFrame(() => {
-        afLoop(af, workLength, startTime)
-    });
-}
 
-function setActiveButton (target) {
-    document.querySelectorAll('#controls .button.active').forEach((activeButton) => {
-        activeButton.classList.remove('active');
-    });
-    target.classList.add('active');
-}
+    function restartSt () {
+        if (st.len > 0 && st.len < 300) return;
+        var workLength = parseInt(document.getElementById('workLength').value) || 0;
+        var startTime = (new Date()).getTime();
+        st.len = 0;
+        st.timeLabel.innerHTML = '';
+        stLoop(st, workLength, startTime);
+    }
 
-init();
+    function restartAf () {
+        if (af.len > 0 && af.len < 300) return;
+        var workLength = parseInt(document.getElementById('workLength').value) || 0;
+        var startTime = (new Date()).getTime();
+        af.len = 0;
+        af.timeLabel.innerHTML = '';
+        afLoop(af, workLength, startTime);
+    }
+
+    function stLoop (st, workLength, startTime) {
+        if (!render(st, workLength, startTime)) {
+            restartAf();
+            return false;
+        }
+        setTimeout(() => {
+            stLoop(st, workLength, startTime)
+        }, 16.667);
+    }
+
+    function afLoop (af, workLength, startTime) {
+        if (!render(af, workLength, startTime)) {
+            return false;
+        }
+        requestAnimationFrame(() => {
+            afLoop(af, workLength, startTime)
+        });
+    }
+
+    function setActiveButton (target) {
+        document.querySelectorAll('#controls .button.active').forEach((activeButton) => {
+            activeButton.classList.remove('active');
+        });
+        target.classList.add('active');
+    }
+
+    init();
+})();
